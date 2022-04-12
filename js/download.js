f97569f634dbdfb459f9500a959a5356596d6e83
llontopdev <llontopdev@gmail.com>
2022-04-12 15:34:12 -0300

    confirmar en master

--- js/download.js
+++ js/download.js
@@ -1,15 +1,16 @@
 //Definimos el botón para escuchar su click
 const $boton = document.querySelector("#btnCapturar"), // El botón que desencadena
-$objetivo = document.querySelector("#signature"); // A qué le tomamos la foto
+  $objetivo = document.querySelector("#signature"); // A qué le tomamos la foto
 
 // Agregar el listener al botón
 $boton.addEventListener("click", () => {
   html2canvas($objetivo) // Llamar a html2canvas y pasarle el elemento
     .then(canvas => {
+      let nombre = document.querySelector(".firma_nombre").innerHTML.replace(" ","_");      
       // Cuando se resuelva la promesa traerá el canvas
       // Crear un elemento <a>
       let enlace = document.createElement('a');
-      enlace.download = "Signature.png";
+      enlace.download = `Signature_${nombre}.png`;
       // Convertir la imagen a Base64
       enlace.href = canvas.toDataURL();
       // Hacer click en él
